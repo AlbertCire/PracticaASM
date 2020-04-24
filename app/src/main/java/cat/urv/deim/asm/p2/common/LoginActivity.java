ebe6652ed22cf5f785d8cf5763d83d9676bc2800
AlbertCire <albertcire@gmail.com>
2020-04-24 12:01:23 +0200

    drawable

---
+++ app/src/main/java/cat/urv/deim/asm/p2/common/LoginActivity.java
@@ -0,0 +1,13 @@
+package cat.urv.deim.asm.p2.common;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
