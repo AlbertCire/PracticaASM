9f8650a70465d5e27c70139de46a39f290c6b6ca
AlbertCire <albertcire@gmail.com>
2020-04-24 11:46:17 +0200

    splash i help_1

--- app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SplashActivity.java
@@ -2,13 +2,31 @@ package cat.urv.deim.asm.p2.common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private final int DURACIO_SPLASH = 2000;    //Duracion de la SplashActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MenuActivity.class); //MenuActivity es la siguiente activity despues del Splash
+                startActivity(intent);
+                finish();
+            };
+        }, DURACIO_SPLASH);
     }
 }
---
+++ app/src/main/java/cat/urv/deim/asm/p2/common/TutorialActivity.java
@@ -0,0 +1,17 @@
+package cat.urv.deim.asm.p2.common;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class TutorialActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
